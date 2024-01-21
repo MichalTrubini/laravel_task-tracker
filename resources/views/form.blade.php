48fb4eab8e85720db74fd2928adeba1a55ccad24
Michal Trubini <mchl.trubini@gmail.com>
2024-01-21 19:25:40 +0100

    project

---
+++ resources/views/form.blade.php
@@ -0,0 +1,40 @@
+@section('title', isset($task) ? 'Edit Task' : 'Add Task')
+
+@section('content')
+
+<form method="POST" action="{{isset($task) ? route('tasks.update', ['task' => $task->id]) : route('tasks.store')}}">
+@csrf
+@isset($task)
+    @method('PUT')
+@endisset
+
+<div class="mb-4">
+    <label for="title">Title</label>
+    <input @class(['border-red-500' => $errors->has('title')]) type="text" name="title" id="title" value="{{$task->title ?? old('title')}}">
+    @error('title')
+        <p class="error">{{$message}}</p>
+    @enderror
+</div>
+
+<div class="mb-4">
+    <label for="description">Description</label>
+    <textarea @class(['border-red-500' => $errors->has('description')]) type="text" name="description" id="description" rows="5">{{$task->description ?? old('description')}}</textarea>
+    @error('description')
+    <p class="error">{{$message}}</p>
+@enderror
+</div>
+
+<div class="mb-4">
+    <label for="long_description">Long Description</label>
+    <textarea @class(['border-red-500' => $errors->has('long_description')]) type="text" name="long_description" id="long_description" rows="10">{{$task->long_description ?? old('long_description')}}</textarea>
+    @error('long_description')
+    <p class="error">{{$message}}</p>
+@enderror
+</div>
+
+<div class="flex gap-2">
+    <button class="btn" type="submit">@isset($task) Update Task @else Add Task @endisset</button>
+    <a href="{{route('tasks.index')}}" class="btn">Cancel</a>
+</div>
+</form>
+@endsection
